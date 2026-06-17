7254b45ac40109a1fbc7462eb99fa4ca99ace3c6
Tim Gerundt <tim.gerundt@montanhydraulik.com>
2026-06-17 09:22:14 +0200

    Use new github URL

--- index.php
+++ index.php
@@ -67,15 +67,15 @@ Cppcheck is designed to be able to analyze your C/C++ code even if it has non-st
   </tr>
   <tr>
     <td>Windows 64-bit (No XP support)</td>
-    <td><a href="https://github.com/danmar/cppcheck/releases/download/2.20.0/cppcheck-2.20.0-x64-Setup.msi">Installer</a></td>
+    <td><a href="https://github.com/cppcheck-opensource/cppcheck/releases/download/2.20.0/cppcheck-2.20.0-x64-Setup.msi">Installer</a></td>
   </tr>
   <tr>
     <td>Source code (.zip)</td>
-    <td><a href="https://github.com/danmar/cppcheck/archive/2.20.0.zip">Archive</a></td>
+    <td><a href="https://github.com/cppcheck-opensource/cppcheck/archive/2.20.0.zip">Archive</a></td>
   </tr>
   <tr>
     <td>Source code (.tar.gz)</td>
-    <td><a href="https://github.com/danmar/cppcheck/archive/2.20.0.tar.gz">Archive</a></td>
+    <td><a href="https://github.com/cppcheck-opensource/cppcheck/archive/2.20.0.tar.gz">Archive</a></td>
   </tr>
 </table>
 
